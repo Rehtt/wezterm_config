3ef1ea616607ecac891d9a15f77a5210d9b11b2f
Rehtt <dsreshiram@gmail.com>
2026-05-29 09:54:00 +0800

    feat: 修改windows wsl默认

--- wezterm.lua
+++ wezterm.lua
@@ -44,11 +44,11 @@ config.hide_tab_bar_if_only_one_tab = false
 -- =========================================================
 
 if is_windows then
-	config.default_prog = { "wsl.exe", "--distribution", "Arch" }
+	config.default_prog = { "wsl.exe"}
 	config.launch_menu = {
 		{ label = "PowerShell Core", args = { "pwsh.exe", "-NoLogo" } },
 		{ label = "Windows PowerShell", args = { "powershell.exe", "-NoLogo" } },
-		{ label = "WSL (Ubuntu)", args = { "wsl.exe", "--distribution", "Arch" } },
+		{ label = "WSL", args = { "wsl.exe" } },
 		{ label = "Command Prompt", args = { "cmd.exe" } },
 	}
 end
